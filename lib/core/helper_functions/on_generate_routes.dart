3da773aea5d40d2693ba1a6d47f94ee40c9a7b8c
Chemseddine25 <chemseddine.adjas.umba@gmail.com>
2026-02-12 16:22:33 +0100

    routes navigation views

--- lib/core/helper_functions/on_generate_routes.dart
+++ lib/core/helper_functions/on_generate_routes.dart
@@ -1,3 +1,4 @@
+import 'package:dechbord_fruit_app/features/add_product/presentation/views/add_product_view.dart';
 import 'package:dechbord_fruit_app/features/dashboard/presentation/views/dashboard.dart';
 import 'package:flutter/material.dart';
 
@@ -5,6 +6,9 @@ Route<dynamic> onGenerateRoute(RouteSettings settings) {
   switch (settings.name) {
     case DashboardView.routeName:
       return MaterialPageRoute(builder: (context) => const DashboardView());
+
+    case AddProductView.routeName:
+      return MaterialPageRoute(builder: (context) => const AddProductView());
 
     default:
       return MaterialPageRoute(builder: (context) => const Scaffold());
