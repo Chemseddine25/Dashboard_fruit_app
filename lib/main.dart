aad93ca9d0116a95affb228f8f74b339dca32352
Chemseddine25 <chemseddine.adjas.umba@gmail.com>
2026-02-12 15:55:44 +0100

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
+import 'package:bloc/bloc.dart';
+import 'package:dechbord_fruit_app/core/errors/services/bloc_observer.dart';
+import 'package:dechbord_fruit_app/core/errors/services/get_it.dart';
+import 'package:dechbord_fruit_app/core/errors/services/supbase_storage.dart';
 import 'package:dechbord_fruit_app/core/helper_functions/on_generate_routes.dart';
 import 'package:dechbord_fruit_app/features/dashboard/presentation/views/dashboard.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  Bloc.observer = CustomBlocObserver();
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await StorageSupbaseService.initSupabase();
+
+  setupGetit();
+
   runApp(const MyApp());
 }
 
