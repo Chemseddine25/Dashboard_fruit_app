95d79daa2c4c09c819a59ebd20b7afb3e83667e4
Chemseddine25 <chemseddine.adjas.umba@gmail.com>
2026-02-12 16:28:46 +0100

    feat: add product implents repos

--- lib/features/add_product/presentation/views/widgets/repos/product_repos/add_product_impl_repo.dart
+++ lib/features/add_product/presentation/views/widgets/repos/product_repos/add_product_impl_repo.dart
@@ -2,8 +2,8 @@ import 'dart:developer';
 
 import 'package:dartz/dartz.dart';
 import 'package:dechbord_fruit_app/core/errors/failures.dart';
-import 'package:dechbord_fruit_app/core/services/database_service.dart';
-import 'package:dechbord_fruit_app/features/add_product/data/models/add_product_model.dart';
+import 'package:dechbord_fruit_app/core/errors/services/database_service.dart';
+import 'package:dechbord_fruit_app/features/add_product/data/models/add_products_model.dart';
 import 'package:dechbord_fruit_app/features/add_product/domain/entities/add_product_entity.dart';
 import 'package:dechbord_fruit_app/features/add_product/presentation/views/widgets/repos/product_repos/add_product_repo.dart';
 
@@ -19,7 +19,8 @@ class AddProductImplRepo extends AddProductRepo {
           'products', AddProductModel.fromEntity(addProductEntity).toJson());
       return const Right(null);
     } catch (e) {
-      log('❌ Database Technical Error: $e');
+      log('❌ Supabase Technical Error: $e');
+
       return Left(ServerFailure('Failed to add product'));
     }
   }
